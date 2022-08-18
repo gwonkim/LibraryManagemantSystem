4bbb5299ec75bee704e7aba75b1f61bdca975f31
kim, jiwon <wo41896804@gmail.com>
2022-08-18 21:45:51 +0900

    book, return controller
    return 값 형식 변경

--- src/main/java/com/lms/controller/BookController.java
+++ src/main/java/com/lms/controller/BookController.java
@@ -10,21 +10,59 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 
 import com.lms.entity.Book;
-import com.lms.entity.User;
 import com.lms.repository.BookRepository;
-import com.lms.repository.UserRepository;
+import com.lms.repository.DepartmentRepository;
 
 @Controller
 @RestController
 @RequestMapping("book")
 public class BookController {
     @Autowired BookRepository bookRepository;
+    @Autowired DepartmentRepository departmentRepository;
 
     // 책목록
     @RequestMapping("list")
-    public List<Book> list(Model model) {
-        return bookRepository.findAll();
+    public ResponseEntity<List<Book>> list(Model model) {
+    	return ResponseEntity.ok().body(bookRepository.findAll());
     }
+    
+    // 신규 서적 등록
+    @GetMapping("register")
+    public ResponseEntity<Book> create(Model model) {
+        Book book = new Book();
+        // return bookRepository.findAll();
+        return ResponseEntity.ok().body(book);
+    }
+
+    @PostMapping("register")
+    public String create(Model model, Book book) {
+    	bookRepository.save(book);
+        return "redirect:list";
+    }
+
+    // 서적 수정
+    @GetMapping("edit")
+    public ResponseEntity<Book> edit(Model model, @RequestParam("id") int id) {
+        Book book = bookRepository.findById(id).get();
+        // List<Department> departments = departmentRepository.findAll();
+        return ResponseEntity.ok().body(book);
+    }
+
+    @PostMapping("edit")
+    public String edit(Model model, Book book) {
+        bookRepository.save(book);
+        return "redirect:list";
+    }
+
+    @RequestMapping("delete")
+    public String delete(Model model, @RequestParam("id") int id) {
+    	bookRepository.deleteById(id);
+        return "redirect:list";
+    }
+
 }
--- src/main/java/com/lms/controller/BookReturnController.java
+++ src/main/java/com/lms/controller/BookReturnController.java
@@ -3,6 +3,7 @@ package com.lms.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,17 +11,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.lms.entity.Book;
 import com.lms.entity.BookReturn;
 import com.lms.repository.BookReturnRepository;
 
 @RestController
 @RequestMapping("return")
 public class BookReturnController {
-    @Autowired BookReturnRepository BookReturnRepository;
+    @Autowired BookReturnRepository bookReturnRepository;
 
     // 책목록
     @RequestMapping("list")
-    public List<BookReturn> find(Model model) {
-        return BookReturnRepository.findAll();
+    public ResponseEntity<List<BookReturn>> find(Model model) {
+    	return ResponseEntity.ok().body(bookReturnRepository.findAll());
     }
 }

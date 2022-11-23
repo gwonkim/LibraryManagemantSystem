42b7e37c6b201a46e1ca065bf07a7ee8583da195
kim, jiwon <wo41896804@gmail.com>
2022-11-24 00:07:56 +0900

    [book] 검색 css 수정

--- src/main/resources/static/res/book.js
+++ src/main/resources/static/res/book.js
@@ -1,7 +1,7 @@
 const bookResponsive = function () {
   let form = $("form");
   let content = $("section").children("#content");
-    if ($(window).width() < 901) {
+    if ($(window).width() < 1001) {
       content.addClass("contentsM");
       content.removeClass("contents");
     } else {
